f00af02b4200e0b687bb24a2f2b1c7190fb96371
Ubuntu <ubuntu@ip-172-31-7-8.ec2.internal>
2021-09-14 20:08:40 +0000

    rectangle area and perimeter user input

---
+++ rectangle_a_p_user_input.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Created by: Michael Zagon
+# Created on: Sep 2021
+# This program calculates the area and perimeter of a rectangle with
+# with user input
+
+
+def main():
+    # this function calculates the area and perimeter
+
+    # input
+    length = int(input("Enter length of the rectangle (mm): "))
+    width = int(input("Enter width of the rectangle (mm): "))
+
+    # process
+    area = length * width
+    perimeter = 2 * (length + width)
+
+    # output
+    print("")
+    print("Area is {0} mm².".format(area))
+    print("Perimeter is {0} mm.".format(perimeter))
+
+
+if __name__ == "__main__":
+    main()
